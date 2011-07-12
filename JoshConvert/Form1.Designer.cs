4de558ed8a3bd4fe167fddbe3d5c4ba20b5b34cc
Josh Harle <josh.harle@gmail.com>
2011-07-12 18:58:52 +1000

    first commit

---
+++ JoshConvert/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tRCfile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tInputDAE = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tOutputFolder = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tMeshName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tCrysisResourceFolder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tRCfile
+            // 
+            this.tRCfile.Location = new System.Drawing.Point(12, 31);
+            this.tRCfile.Name = "tRCfile";
+            this.tRCfile.Size = new System.Drawing.Size(260, 20);
+            this.tRCfile.TabIndex = 1;
+            this.tRCfile.Text = "D:\\Crysis Wars\\Bin32\\rc\\rc.exe";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "RC file location";
+            // 
+            // tInputDAE
+            // 
+            this.tInputDAE.Location = new System.Drawing.Point(12, 84);
+            this.tInputDAE.Name = "tInputDAE";
+            this.tInputDAE.Size = new System.Drawing.Size(260, 20);
+            this.tInputDAE.TabIndex = 3;
+            this.tInputDAE.Text = "d:\\temp\\phil.DAE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input DAE location";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tOutputFolder
+            // 
+            this.tOutputFolder.Location = new System.Drawing.Point(12, 121);
+            this.tOutputFolder.Name = "tOutputFolder";
+            this.tOutputFolder.Size = new System.Drawing.Size(260, 20);
+            this.tOutputFolder.TabIndex = 5;
+            this.tOutputFolder.Text = "d:\\temp\\";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output Folder";
+            // 
+            // tMeshName
+            // 
+            this.tMeshName.Location = new System.Drawing.Point(12, 162);
+            this.tMeshName.Name = "tMeshName";
+            this.tMeshName.Size = new System.Drawing.Size(260, 20);
+            this.tMeshName.TabIndex = 7;
+            this.tMeshName.Text = "Phil";
+            this.tMeshName.TextChanged += new System.EventHandler(this.tMeshName_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Mesh Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Crysis Resource Folder";
+            // 
+            // tCrysisResourceFolder
+            // 
+            this.tCrysisResourceFolder.Enabled = false;
+            this.tCrysisResourceFolder.Location = new System.Drawing.Point(14, 211);
+            this.tCrysisResourceFolder.Name = "tCrysisResourceFolder";
+            this.tCrysisResourceFolder.Size = new System.Drawing.Size(260, 20);
+            this.tCrysisResourceFolder.TabIndex = 9;
+            this.tCrysisResourceFolder.Text = "Objects/phil/";
+            this.tCrysisResourceFolder.TextChanged += new System.EventHandler(this.tCrysisResourceFolder_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 427);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tCrysisResourceFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tMeshName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tOutputFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tInputDAE);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tRCfile);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tRCfile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tInputDAE;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tOutputFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tMeshName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tCrysisResourceFolder;
+    }
+}
+
---
+++ JoshConvert/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        struct MeshSource
+        {
+            public string Name;
+            public string floatCount;
+            public string floatArrayContent;
+            public string accessorCount;
+
+            public MeshSource(string _Name, string _floatCount, string _floatArrayContent, string _accessorCount) 
+           {
+                this.Name = _Name;             
+                this.floatCount = _floatCount;
+                this.floatArrayContent = _floatArrayContent;
+                this.accessorCount = _accessorCount;
+           }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string outputDAE = tOutputFolder.Text + tMeshName.Text + "_cloud0.dae";
+            string outputDAEname = tMeshName.Text + "_cloud0";
+
+            List<string> cloudFiles = new List<string>();
+            cloudFiles.Add(outputDAEname);
+
+
+            if (!convertDAE(tInputDAE.Text, tMeshName.Text + "_cloud0", tCrysisResourceFolder.Text, outputDAE))
+                return;
+ 
+            if (!runThroughRC(outputDAE))
+                return;
+
+            if (!buildPrefab(tMeshName.Text, cloudFiles, tOutputFolder.Text, tCrysisResourceFolder.Text))
+                return;
+
+
+
+            
+        }
+
+
+
+        private bool convertDAE(string inputDAE, string cloudname, string imagelocation, string outputDAE)
+        {
+            // open input DAE
+            string s = System.IO.File.ReadAllText(inputDAE);
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(s);
+            XmlNode xnRoot = xml.ChildNodes[1];
+
+
+            // open template
+            string template = System.IO.File.ReadAllText(Application.StartupPath + "\\template\\crysis_dae.xml");
+
+
+            // now fill out data in each mesh source
+            Hashtable sources = new Hashtable();
+            foreach (XmlNode meshsource in xnRoot["library_geometries"]["geometry"]["mesh"].ChildNodes)
+            {
+                if (meshsource.Name == "source")
+                {
+                    sources.Add(meshsource.Attributes["id"].InnerText.ToString(), new MeshSource(meshsource.Attributes["id"].InnerText.ToString(), meshsource["float_array"].Attributes["count"].InnerText.ToString(), meshsource["float_array"].InnerText.ToString(), meshsource["technique_common"]["accessor"].Attributes["count"].InnerText.ToString()));
+                }
+            }
+
+            // look up TEXCOORD, NORMAL, and VERTEX/POSITION elements properly (not assuming fixed order)
+            Hashtable lookup = new Hashtable();
+
+
+            // is the node called polygons or triangles?
+            string polygonName;
+            if (xnRoot["library_geometries"]["geometry"]["mesh"].GetElementsByTagName("triangles").Count == 1)
+            {
+                polygonName = "triangles";
+            }
+            else if (xnRoot["library_geometries"]["geometry"]["mesh"].GetElementsByTagName("polygons").Count == 1)
+            {
+                polygonName = "polygons";
+            } else {
+                MessageBox.Show("No Triangle/Polygon node found!");
+                return false;
+            }
+
+
+            foreach (XmlNode source in xnRoot["library_geometries"]["geometry"]["mesh"][polygonName].ChildNodes)
+            {
+                if (source.Name == "input") 
+                    lookup.Add(source.Attributes["semantic"].InnerText.ToString(), source.Attributes["source"].InnerText.ToString().Replace("#", ""));              
+            }
+            // lookup position source from vertext
+            string SourcePosName = xnRoot["library_geometries"]["geometry"]["mesh"]["vertices"].Attributes["id"].InnerText.ToString();
+
+            if (SourcePosName == lookup["VERTEX"].ToString())
+            {
+                lookup.Add("POSITION", xnRoot["library_geometries"]["geometry"]["mesh"]["vertices"]["input"].Attributes["source"].InnerText.ToString().Replace("#", ""));
+            }
+            else
+            {
+                MessageBox.Show("Error: Could not find POSITION mesh in vertices node");
+                return false;
+            }
+
+            string triangleCount = xnRoot["library_geometries"]["geometry"]["mesh"][polygonName].Attributes["count"].InnerText.ToString();
+            // combined all <p> nodes together
+            string pContent = "";
+            foreach (XmlNode node in xnRoot["library_geometries"]["geometry"]["mesh"][polygonName].GetElementsByTagName("p")) {
+                pContent = pContent + node.InnerText + " ";
+            }
+
+
+
+            // Now populate the template
+            template = template.Replace("(created_date)", DateTime.UtcNow.ToString("s") + "Z");
+            template = template.Replace("(modified_date)", DateTime.UtcNow.ToString("s") + "Z");
+            template = template.Replace("(imagefolder)", imagelocation);
+            
+            template = template.Replace("(imagename)", (Path.GetFileName(xnRoot["library_images"]["image"].InnerText)));
+            template = template.Replace("(cloudname)", cloudname);
+
+            template = template.Replace("(pos_count)", ((MeshSource)sources[lookup["POSITION"].ToString()]).floatCount);
+            template = template.Replace("(pos_content)", ConvertWhitespaceToSpacesRegex(((MeshSource)sources[lookup["POSITION"].ToString()]).floatArrayContent));
+            template = template.Replace("(pos_assessor_count)",  ((MeshSource)sources[lookup["POSITION"].ToString()]).accessorCount);
+            template = template.Replace("(normal_count)", ((MeshSource)sources[lookup["NORMAL"].ToString()]).floatCount);
+            template = template.Replace("(normal_content)", ConvertWhitespaceToSpacesRegex(((MeshSource)sources[lookup["NORMAL"].ToString()]).floatArrayContent));
+            template = template.Replace("(normal_assessor_count)", ((MeshSource)sources[lookup["NORMAL"].ToString()]).accessorCount);
+            template = template.Replace("(texcoord_count)", ((MeshSource)sources[lookup["TEXCOORD"].ToString()]).floatCount);
+            template = template.Replace("(texcoord_content)", ConvertWhitespaceToSpacesRegex(((MeshSource)sources[lookup["TEXCOORD"].ToString()]).floatArrayContent ));
+            template = template.Replace("(texcoord_assessor_count)", ((MeshSource)sources[lookup["TEXCOORD"].ToString()]).accessorCount);
+            template = template.Replace("(triangle_count)", triangleCount);
+            template = template.Replace("(triangle_content)", ConvertWhitespaceToSpacesRegex(pContent));
+
+
+
+
+          
+            // write out template file
+            TextWriter tw = new StreamWriter(outputDAE);
+            tw.Write(template);
+            tw.Close();
+
+
+           // MessageBox.Show("Done converting DAE.  Now running RC");
+            return true;
+   
+        }
+
+        private bool runThroughRC(string filename) { 
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.EnableRaisingEvents=false;
+            proc.StartInfo.FileName=tRCfile.Text;
+            proc.StartInfo.Arguments = "\"" + filename+"\"";
+            proc.Start();
+            proc.WaitForExit();
+            //MessageBox.Show("RC processed");
+            return true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tMeshName_TextChanged(object sender, EventArgs e)
+        {
+            tCrysisResourceFolder.Text = "Objects/"+ tMeshName.Text + "/";
+        }
+
+        private void tCrysisResourceFolder_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool buildPrefab(string MeshName, List<string> cloudFiles, string outputFolder, string CrysisResourceFolder)
+        {
+
+            // write out prefab file
+            TextWriter tw = new StreamWriter(outputFolder + "/" + MeshName + "_prefab.xml");
+
+
+
+
+            Guid uuid = Guid.NewGuid();
+
+            tw.Write("<PrefabsLibrary Name=\""+MeshName+"_prefab\">\n");
+
+            tw.Write(" <Prefab Name=\""+MeshName+"_prefab01\" Id=\"{"+uuid.ToString()+"}\">\n");
+            tw.Write("  <Objects>\n");
+
+            foreach (string cloudname in cloudFiles)
+            {
+                tw.Write("   <Object ");
+                tw.Write("Type=\"Brush\" ");
+                tw.Write("Layer=\"Main\" ");
+                uuid = Guid.NewGuid();
+                tw.Write("Id=\"{"+uuid.ToString()+"}\" ");
+                tw.Write("Name=\""+cloudname+"\" ");
+                tw.Write("Pos=\"0,0,0\" ");
+                tw.Write("Rotate=\"0,0,0,0\" ");
+                tw.Write("ColorRGB=\"16777215\" ");
+                tw.Write("MatLayersMask=\"0\" ");
+                tw.Write("Prefab=\""+CrysisResourceFolder+cloudname+".cfg\" ");
+                tw.Write("OutdoorOnly=\"0\" ");
+                tw.Write("CastShadowMaps=\"1\" ");
+                tw.Write("SupportSecondVisarea=\"0\" ");
+                tw.Write("CastRAMmap=\"0\" ");
+                tw.Write("ReceiveRAMmap=\"0\" ");
+                tw.Write("Hideable=\"0\" ");
+                tw.Write("LodRatio=\"100\" ");
+                tw.Write("ViewDistRatio=\"100\" ");
+                tw.Write("NotTriangulate=\"0\" ");
+                tw.Write("AIRadius=\"-1\" ");
+                tw.Write("NoStaticDecals=\"0\" ");
+                tw.Write("RAMmapQuality=\"1\" ");
+                tw.Write("NoAmnbShadowCaster=\"0\" ");
+                tw.Write("RecvWind=\"0\" ");
+                tw.Write("RndFlags=\"8\"");
+                tw.Write("/>\n");
+            }
+
+            tw.Write("  </Objects>\n");
+            tw.Write(" </Prefab>\n");
+            tw.Write("</PrefabsLibrary>\n");
+
+
+
+            tw.WriteLine();
+            tw.Close();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Converts all whitespace in the string to spaces using Regex.
+        /// </summary>
+        public static string ConvertWhitespaceToSpacesRegex(string value)
+        {
+            value = Regex.Replace(value, "[\n\r\t]", " ");
+            return value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "\\settings.txt"))
+            {
+                loadSettings();
+            }
+            else
+            {
+                saveSettings();
+            }
+        }
+
+        private void loadSettings()
+        {
+            List<string> settings = (System.IO.File.ReadAllLines(Application.StartupPath + "\\settings.txt")).ToList();
+            tRCfile.Text = settings[0];
+            tInputDAE.Text = settings[1];
+            tOutputFolder.Text = settings[2];
+            tMeshName.Text = settings[3];
+        }
+
+        private void saveSettings()
+        {
+            // compile settings
+            string settings = tRCfile.Text + "\n" + tInputDAE.Text + "\n" + tOutputFolder.Text + "\n" + tMeshName.Text;
+
+
+            // write out settings
+            TextWriter tw = new StreamWriter(Application.StartupPath + "\\settings.txt");
+            tw.Write(settings);
+            tw.Close();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveSettings();
+        }
+
+    }
+}
